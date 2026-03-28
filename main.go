87af54993c45b6242246c73c0f55806a77cc9c4e
tany <kolodnikovatata@gmail.com>
2026-03-28 12:18:50 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Response struct {
+	DaysUntilNewYear int    `json:"days_until_new_year"`
+	Message          string `json:"message"`
+}
+
+func daysUntilNewYear() int {
+	now := time.Now()
+	year := now.Year()
+	newYear := time.Date(year+1, time.January, 1, 0, 0, 0, 0, time.Local)
+
+	duration := newYear.Sub(now)
+	days := int(duration.Hours() / 24)
+
+	return days
+}
+
+func newYearHandler(w http.ResponseWriter, r *http.Request) { //обьект для отправки ответа и обьект с данными запроса от браузера
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	days := daysUntilNewYear()
+
+	response := Response{
+		DaysUntilNewYear: days,
+		Message:          "До нового года осталось дней, надеюсь доживем",
+	}
+
+	json.NewEncoder(w).Encode(response) //создаем то что будет писать в ответе response json
+}
+
+func main() {
+	http.HandleFunc("/", newYearHandler)
+
+	port := "3000"
+	log.Printf("Сервер запущен на порту %s\n", port) //подставляет значение переменной порт
+	log.Printf("Перейдите на http://localhost:%s\n", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, nil)) //вывод ошибки и закрытие проги
+}
